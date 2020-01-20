6634e58737493f02336dce4158515914d78d307a
julien <j.graisse@gmail.com>
2020-01-20 21:33:15 +0100

    upgrade to new version (six module deprecated)

--- users/tokens.py
+++ users/tokens.py
@@ -1,9 +1,8 @@
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
-from django.utils import six
 class TokenGenerator(PasswordResetTokenGenerator):
     def _make_hash_value(self, user, timestamp):
         return (
-            six.text_type(user.pk) + six.text_type(timestamp) +
-            six.text_type(user.is_active)
+            user.pk + timestamp +
+            user.is_active
         )
 account_activation_token = TokenGenerator()
